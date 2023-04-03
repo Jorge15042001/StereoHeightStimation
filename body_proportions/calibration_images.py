857ad54732c1caf573c5948af2c6f3134653f5f5
Jorge Vulgarin <adrivulgarin@hotmail.com>
2023-04-02 20:28:11 -0500

    refactor capture calibration images code

--- body_proportions/calibration_images.py
+++ body_proportions/calibration_images.py
@@ -1,5 +1,6 @@
 import cv2
-from cameraArray import CamArray 
+from cameraArray import CamArray
+from datetime import datetime
 import os
 
 images_dir = "./images"
@@ -7,49 +8,51 @@ if not os.path.exists(images_dir):
     os.makedirs(images_dir)
 
 
-
 left_cam_idx = 2
 right_cam_idx = 4
-cams = CamArray([left_cam_idx,right_cam_idx])
+cams = CamArray([left_cam_idx, right_cam_idx])
 cams.start()
 
-num = 0
+def flip_correction(img):
+    return img[::-1][:, ::-1]
 
-def flip_correction (img):
-    return img [::-1][:,::-1]
 
-def save_images(img1,img2,num):
-    cv2.imwrite(f'{images_dir}/imageL' + str(num) + '.png', img1)
-    cv2.imwrite(f'{images_dir}/imageR' + str(num) + '.png', img2)
+def save_images(img1, img2):
+    now = datetime.now()
+    timestamp = now.strftime("%m.%d.%Y_%H:%M:%S.%f")
+    cv2.imwrite(f'{images_dir}/imageL__{timestamp}.png', img1)
+    cv2.imwrite(f'{images_dir}/imageR__{timestamp}.png', img2)
     print("images saved!")
 
-def preview_and_save(img1,img2,num):
+
+def preview_and_save(img1, img2):
     while True:
         k2 = cv2.waitKey(5)
-        if k2 == ord("q"): break
-        if k2 == ord("s"): 
-            save_images(img, img2, num)
-            num += 1
+        if k2 == ord("q"):
+            break
+        if k2 == ord("s"):
+            save_images(img1, img2)
             break
 
-while cams.isOpened():
 
-    #  succes1, img = cap.read()
-    #  succes2, img2 = cap2.read()
-    frame1,frame2 = cams.get_frames()
-    _,img= frame1
-    _,img2= frame2
-    #  img = flip_correction(img)
-    #  img2 = flip_correction(img2)
+def capture_images(cams:CamArray):
+    if len(cams.cams) !=2:
+        print(f"No support for {len(cams.cams)} cameras, only use 2 cameras ")
+        exit(-1)
 
 
-    k = cv2.waitKey(5)
+    while cams.isOpened():
+        frame_left, frame_right = cams.get_frames()
+        _, img_left = frame_left
+        _, img_right = frame_right
 
-    if k == 27:
-        break
-    if k == ord('s'): # preview the image
-        preview_and_save(img, img2, num)
-        num += 1
+        k = cv2.waitKey(5)
 
-    cv2.imshow('Img 1',img)
-    cv2.imshow('Img 2',img2)
+        if k == ord('q'):
+            break
+
+        if k == ord('s'):  # preview the image
+            preview_and_save(img_left, img_right)
+
+        cv2.imshow('Img 1', img_left)
+        cv2.imshow('Img 2', img_right)
