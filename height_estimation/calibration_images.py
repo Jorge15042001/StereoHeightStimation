8ee9a470978cd10c2aa792ab252b3fd06cf6467a
Jorge Vulgarin <adrivulgarin@hotmail.com>
2023-05-07 15:29:37 -0500

    receive path from terminal

--- height_estimation/calibration_images.py
+++ height_estimation/calibration_images.py
@@ -2,18 +2,18 @@ import cv2
 from cameraArray import CamArray
 from datetime import datetime
 from utils import loadCamArrayFromJson
-import os
+import sys
 
-images_dir = "./images"
-if not os.path.exists(images_dir):
-    os.makedirs(images_dir)
+#  images_dir = "./images"
+#  if not os.path.exists(images_dir):
+#      os.makedirs(images_dir)
 
 
 def flip_correction(img):
     return img[::-1][:, ::-1]
 
 
-def save_images(img1, img2):
+def save_images(images_dir, img1, img2):
     now = datetime.now()
     timestamp = now.strftime("%m.%d.%Y_%H:%M:%S.%f")
     cv2.imwrite(f'{images_dir}/imageL__{timestamp}.png', img1)
@@ -21,21 +21,20 @@ def save_images(img1, img2):
     print("images saved!")
 
 
-def preview_and_save(img1, img2):
+def preview_and_save(images_dir, img1, img2):
     while True:
         k2 = cv2.waitKey(5)
         if k2 == ord("q"):
             break
         if k2 == ord("s"):
-            save_images(img1, img2)
+            save_images(images_dir, img1, img2)
             break
 
 
-def capture_images(cams:CamArray):
-    if len(cams.cams) !=2:
+def capture_images(cams: CamArray, images_dir):
+    if len(cams.cams) != 2:
         print(f"No support for {len(cams.cams)} cameras, only use 2 cameras ")
         exit(-1)
-
 
     while cams.isOpened():
         frame_left, frame_right = cams.get_frames()
@@ -48,14 +47,18 @@ def capture_images(cams:CamArray):
             break
 
         if k == ord('s'):  # preview the image
-            preview_and_save(img_left, img_right)
+            preview_and_save(images_dir, img_left, img_right)
 
         cv2.imshow('Img 1', img_left)
         cv2.imshow('Img 2', img_right)
 
 
 if __name__ == "__main__":
-    cams = loadCamArrayFromJson("./stereo_config.json")
+
+    stereo_config_file = sys.argv[1]
+    images_dir = sys.argv[2]
+
+    cams = loadCamArrayFromJson(stereo_config_file)
     cams.start()
-    capture_images(cams)
+    capture_images(cams, images_dir)
     cams.close()
