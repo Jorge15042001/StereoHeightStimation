59a1865274784e23792c9542fc3fe55ba1380c26
Jorge Vulgarin <adrivulgarin@hotmail.com>
2023-05-06 23:56:50 -0500

    Adding multithreading capabilities

--- src/height_stimation.py
+++ src/height_stimation.py
@@ -3,9 +3,11 @@ import numpy as np
 from calibration import getStereoRectifier
 from triangulation import find_depth_from_disparities
 import mediapipe as mp
+import threading
 from cameraArray import CamArray
 from featuresExtractor import FaceFeatures, FeaturesExtractor
 from utils import startCameraArray, loadStereoCameraConfig, StereoConfig
+from time import sleep
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -39,7 +41,8 @@ def computeHeigth2(features: FaceFeatures, pixel_size: float, cam_center):
     return height
 
 
-def putHeightResult(frame_left, frame_right, success_height, height, depth):
+def showHeighResult(frame_left, frame_right, height, depth):
+    success_height = height is not None
 
     if success_height:
         cv2.putText(frame_right, "Depth: " + str(round(depth, 1)) +
@@ -55,52 +58,196 @@ def putHeightResult(frame_left, frame_right, success_height, height, depth):
         cv2.putText(frame_left, "left TRACKING LOST", (75, 50),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
+    cv2.imshow("frame right", cv2.resize(
+        frame_right, (np.array(frame_right.shape[:2][::-1])*1.5).astype(int)))
+    cv2.imshow("frame left", cv2.resize(
+        frame_left, (np.array(frame_right.shape[:2][::-1])*1.5).astype(int)))
+    if cv2.waitKey(5) & 0xFF == 27:
+        return True
+    return False
+
+
+class MoventAnalizer:
+    def __init__(self, once_every_seconds):
+        self.data = []
+        self.threashold: float = 5
+        self.sleep_time: float = once_every_seconds
+        self.on_person_detected = lambda: None
+        self.on_person_leaves = lambda: None
+        self.keep_loop = True
+        self.person_detected = False
+
+        def movement_analizer():
+            while self.keep_loop:
+                total_elements = len(self.data)
+                none_count = self.data.count(None)
+                not_none_count = total_elements - none_count
+
+                if total_elements == 0:
+                    print("No data in movement_analizer")
+                    sleep(self.sleep_time)
+                    continue
+
+                if total_elements < 30:
+                    sleep(self.sleep_time)
+                    continue
+
+                if total_elements == none_count and self.person_detected:
+                    self.person_detected = False
+                    print("No movement detected")
+                    self.data = []
+                    self.on_person_leaves()
+                    sleep(self.sleep_time)
+                    continue
+
+                if not_none_count/total_elements < 0.9:
+                    self.data = []
+                    sleep(self.sleep_time)
+                    continue
+
+                data_not_none = list(filter(lambda x: x is not None, self.data))
+                data_min = min(data_not_none)
+                data_max = max(data_not_none)
+                if data_max-data_min < self.threashold and not self.person_detected :
+                    self.person_detected = True
+                    self.on_person_detected()
+                self.data = []
+                sleep(self.sleep_time)
+
+        self.thread = threading.Thread(target=movement_analizer)
+        self.thread.start()
+
+    def apped_data(self, data_entry):
+        self.data.append(data_entry)
+    def close(self):
+        self.keep_loop = False
+
+
+class HeightDaemon:
+    def __init__(self, config_file):
+        self.stereo_config = loadStereoCameraConfig("./stereo_config.json")
+        #  cam_center_left, cam_center_right = cam_centers
+        self.f_length = min(self.stereo_config.left_camera.fpx,
+                            self.stereo_config.right_camera.fpx)
+        self.cams = startCameraArray(self.stereo_config.left_camera,
+                                     self.stereo_config.right_camera)
+        self.cams.start()
+
+        self.rectify = getStereoRectifier("./stereoMap.xml")
+        self.features_left = FeaturesExtractor()
+        self.features_right = FeaturesExtractor()
+        self.movement_analizer = MoventAnalizer(2)
+
+    def run(self):
+        while self.cams.isOpened():
+            frame_left, frame_right = self.cams.get_frames()
+            succes_left, frame_left = frame_left
+            succes_right, frame_right = frame_right
+
+            if not succes_right or not succes_left:
+                print("Ignoring empty camera frame.")
+                continue
+
+            frame_left, frame_right = self.rectify(frame_left, frame_right)
+
+            features_left = self.features_left.extract_keypts(frame_left)
+            features_right = self.features_right.extract_keypts(frame_right)
+
+            if not features_left[0] or not features_right[0]:
+                self.movement_analizer.apped_data(None)
+
+                terminate = self.showHeighResult(frame_left, frame_right,
+                                                 None, None)
+                if terminate:
+                    break
+                continue
+
+            depth = computeDepth(features_left[2], features_right[2],
+                                 self.stereo_config.cam_separation,
+                                 self.f_length)
+
+            self.movement_analizer.apped_data(depth)
+            px_size = self.stereo_config.depth_to_pixel_size * depth
+            #  height = computeHeigth(features_left[1], px_size)
+            height = computeHeigth2(features_left[1], px_size,
+                                    self.stereo_config.left_camera.center)
+
+            terminate = self.showHeighResult(
+                frame_left, frame_right, height, depth)
+            if terminate:
+                break
+        self.cams.close()
+        self.movement_analizer.close()
+
+    def start(self):
+        threading.Thread(target=self.start).start()
+
+    def set_on_person_detected(self, callback):
+        self.movement_analizer.on_person_detected = callback
+
+    def set_on_person_leaves(self, callback):
+        self.movement_analizer.on_person_leaves = callback
+
+    def showHeighResult(self, frame_left, frame_right, height, depth):
+        if self.stereo_config.show_images:
+            return showHeighResult(frame_left, frame_right, height, depth)
+
+
+def person_detected():
+    print("Person detected")
+
+
+def person_leaves():
+    print("Person leaves")
 
 if __name__ == "__main__":
-    stereo_config = loadStereoCameraConfig("./stereo_config.json")
-    #  cam_center_left, cam_center_right = cam_centers
-    f_length = min(stereo_config.left_camera.fpx,
-                   stereo_config.right_camera.fpx)
-    cams = startCameraArray(stereo_config.left_camera,
-                            stereo_config.right_camera)
-    cams.start()
+    height_daemon = HeightDaemon("./stereo_config.json")
+    height_daemon.set_on_person_detected(person_detected)
+    height_daemon.set_on_person_leaves(person_leaves)
+    height_daemon.run()
 
-    rectify = getStereoRectifier("./stereoMap.xml")
-    features_left_extractor = FeaturesExtractor()
-    features_right_extractor = FeaturesExtractor()
-
-    while cams.isOpened():
-        frame_left, frame_right = cams.get_frames()
-        succes_left, frame_left = frame_left
-        succes_right, frame_right = frame_right
-
-        if not succes_right or not succes_left:
-            print("Ignoring empty camera frame.")
-            continue
-
-        frame_left, frame_right = rectify(frame_left, frame_right)
-
-        features_left = features_left_extractor.extract_keypts(frame_left)
-        features_right = features_right_extractor.extract_keypts(frame_right)
-
-        if not features_left[0] or not features_right[0]:
-            continue
-
-        depth = computeDepth(features_left[2], features_right[2],
-                             stereo_config.cam_separation, f_length)
-
-        px_size = stereo_config.depth_to_pixel_size * depth
-        #  height = computeHeigth(features_left[1], px_size)
-        height = computeHeigth2(
-            features_left[1], px_size, stereo_config.left_camera.center)
-
-        putHeightResult(frame_left, frame_right, True, height, depth)
-
-        cv2.imshow("frame right", cv2.resize(
-            frame_right, (np.array(frame_right.shape[:2][::-1])*1.5).astype(int)))
-        cv2.imshow("frame left", cv2.resize(
-            frame_left, (np.array(frame_right.shape[:2][::-1])*1.5).astype(int)))
-        if cv2.waitKey(5) & 0xFF == 27:
-            break
-
-    cams.close()
+    #  stereo_config = loadStereoCameraConfig("./stereo_config.json")
+    #  #  cam_center_left, cam_center_right = cam_centers
+    #  f_length = min(stereo_config.left_camera.fpx,
+    #                 stereo_config.right_camera.fpx)
+    #  cams = startCameraArray(stereo_config.left_camera,
+    #                          stereo_config.right_camera)
+    #  cams.start()
+    #
+    #  rectify = getStereoRectifier("./stereoMap.xml")
+    #  features_left_extractor = FeaturesExtractor()
+    #  features_right_extractor = FeaturesExtractor()
+    #
+    #  while cams.isOpened():
+    #      frame_left, frame_right = cams.get_frames()
+    #      succes_left, frame_left = frame_left
+    #      succes_right, frame_right = frame_right
+    #
+    #      if not succes_right or not succes_left:
+    #          print("Ignoring empty camera frame.")
+    #          continue
+    #
+    #      frame_left, frame_right = rectify(frame_left, frame_right)
+    #
+    #      features_left = features_left_extractor.extract_keypts(frame_left)
+    #      features_right = features_right_extractor.extract_keypts(frame_right)
+    #
+    #      if not features_left[0] or not features_right[0]:
+    #          terminate = showHeighResult(frame_left, frame_right, None, None)
+    #          if terminate:
+    #              break
+    #          continue
+    #
+    #      depth = computeDepth(features_left[2], features_right[2],
+    #                           stereo_config.cam_separation, f_length)
+    #
+    #      px_size = stereo_config.depth_to_pixel_size * depth
+    #      #  height = computeHeigth(features_left[1], px_size)
+    #      height = computeHeigth2(
+    #          features_left[1], px_size, stereo_config.left_camera.center)
+    #
+    #      terminate = showHeighResult(frame_left, frame_right, height, depth)
+    #      if terminate:
+    #          break
+    #
+    #  cams.close()
