693c9912dc12fd7762802c1fb6f99c12ebd09b7b
Jorge10542001 <adrivuglarin@hotmail.com>
2024-01-03 01:13:55 -0500

    remove comments

--- height_estimation/featuresExtractor.py
+++ height_estimation/featuresExtractor.py
@@ -31,9 +31,6 @@ class FaceFeatures:
 
 class FeaturesExtractor:
     def __init__(self, save_pred_to=None):
-        #  self.pose = mp_pose.Pose(model_complexity=1,
-        #                           min_detection_confidence=0.5,
-        #                           min_tracking_confidence=0.5)
         self.save_pred_to = save_pred_to
         self.face_mesh = mp_face_mesh.FaceMesh(
             max_num_faces=4,
@@ -41,36 +38,6 @@ class FeaturesExtractor:
             min_detection_confidence=0.5,
             min_tracking_confidence=0.5
         )
-
-    #  def extract_keypts(self, frame):
-    #      frame_height, frame_width, _ = frame.shape
-    #
-    #      frame.flags.writeable = False  # TODO: does this improve performance?
-    #      frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    #
-    #      results = self.pose.process(frame)
-    #
-    #      frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-    #      frame.flags.writeable = True
-    #
-    #      if not results.pose_landmarks:
-    #          return (False, None)
-    #
-    #      rel_coords = np.array(list(map(lambda pt: (pt.x, pt.y),
-    #                                 results.pose_landmarks.landmark[:11])))
-    #      #  coords = (coords*np.array((frame_width, frame_height))).astype(int)
-    #      img_coords = (rel_coords*np.array((frame_width, frame_height)))
-    #      #  mp_drawing.draw_landmarks(
-    #      #      frame,
-    #      #      results.pose_landmarks,
-    #      #      mp_pose.POSE_CONNECTIONS,
-    #      #      landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
-    #      #  )
-    #      mouth = np.mean([img_coords[9, :], img_coords[10, :]], axis=0)
-    #
-    #      features = FaceFeatures(
-    #          img_coords[2, :], img_coords[5, :], mouth, None)
-    #      return (True, features, img_coords, rel_coords)
 
     def extract_keypts(self, frame):
         frame_height, frame_width, _ = frame.shape
